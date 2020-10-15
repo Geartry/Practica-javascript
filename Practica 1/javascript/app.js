025467cd1dbd60a7c598626eaefe7e35aa7d211b
Geartry <gera_esq_hdz@hotmail.com>
2020-10-15 11:43:18 -0500

    Primer observacion

---
+++ Practica 1/javascript/app.js
@@ -0,0 +1,4 @@
+// Escribir un holo mundo en java escript
+// para imprimir un texto en javascript se utiliza document.write('cadendecaracteres');
+document.write('!hola Mundo¡');
+document.write(3); //para escribir un numero solo se agrega el numero en document.write(3);
