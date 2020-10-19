f7e289989c64b8834babfeb9f5acfff2d0391146
Geartry <gera_esq_hdz@hotmail.com>
2020-10-19 04:30:39 -0500

    tarea o eso creo

---
+++ Practica 2/js/app.js
@@ -0,0 +1,28 @@
+// agregar una funcion con la instruccion function
+function Sumanumeros() {
+
+// declarar las variables
+    let num1 = 5;
+    let num2 = 10;
+    let suma = 0;
+
+    suma = num1 + num2
+
+    // alert("la suma es" + suma)
+
+    alert("hola mundo")
+
+    // despues se hace el proceso  y se imprime
+    document.write(num1 + num2)
+
+    // hacer el proceso y despues la imprime en las configuraciones del navegador
+    console.log(num1 + num2);
+
+    console.log("Hola soy un mensaje que solo se muestra en consola")
+
+}
+
+String = "hola mundo";
+Number = 1, 2, 3, 4;
+Boolean = true
+Boolean = false
---
+++ Tarea/js/resta.js
@@ -0,0 +1,16 @@
+function resta() {
+
+
+    let num1 = 23;
+    let num2 = 8;
+    let rest = 0;
+
+    rest = num1 - num2 
+
+    document.write(num1 - num2)
+
+    console.log(num1 - num2)
+
+    alert("tu resta ha sido identificada como...")
+    alert(num1 - num2)
+}
